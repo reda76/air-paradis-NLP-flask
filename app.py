34cfbe8aafaa110ea1cf11dcf9964a6bf7d2d091
Réda DERRAZ <derraz.mohamedreda@gmail.com>
2022-07-27 17:34:47 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, render_template, request
+from model import predict
+
+
+app = Flask('Twitter Sentiment Analysis')
+
+@app.route('/')
+@app.route('/', methods=['POST'])
+def home():
+    if request.method == 'POST':
+        user_input = request.form["user_tweet"]
+        label = predict(user_input)
+        return render_template('predictform.html', prediction_text = label)
+    return render_template('predictform.html')
+
+
+if __name__ == '__main__':
+    #call the functions here
+    app.run()
